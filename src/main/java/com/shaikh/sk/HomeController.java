c3e539809b53281e035601ef78cbff1ecc2228e0
skwasim777 <wasimsk6061@gmail.com>
2023-10-09 18:03:14 +0530

    first commit

---
+++ src/main/java/com/shaikh/sk/HomeController.java
@@ -0,0 +1,12 @@
+package com.shaikh.sk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home.jsp" ;
+    }
+}
---
+++ src/test/java/com/shaikh/sk/SkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaikh.sk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
